3b9f04bfa876ce81259608e7d1d4187fc3d4b109
Bianca <biancancanal@gmail.com>
2021-02-12 14:05:52 -0600

    commit tree c589000d830c86c87b031905379ea3a09f1d9eee

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    if array.size < 3
+        array.join(" and ")
+    elsif array.size >= 3
+        array_oxford = array
+        array_oxford[-1] = "and #{array_oxford[-1]}"
+        array_oxford.join(", ")
+    end
 end
+
+
+
+
+
+
+
+
+
+    #[arr[0..-2].join(', '), arr.last].join(', and ')
